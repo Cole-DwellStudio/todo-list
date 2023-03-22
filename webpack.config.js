c2020a1311d13ec8b72ed42bf1c5ea3022e38121
Cole-DwellStudio <cole@dwellstudioco.com>
2023-03-22 13:49:02 -0600

    Configure webpack and tailwind

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          sources: {
+            list: [
+              {
+                tag: "img",
+                attribute: "data-src",
+                type: "src",
+              },
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
